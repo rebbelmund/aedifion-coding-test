0c233d7e8d1ae20fb45e98210deffaabf13b327d
rebbelmund <153762544+rebbelmund@users.noreply.github.com>
2024-03-08 03:39:34 +0100

    [feat] test App

---
+++ src/__test__/App.test.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import App from '../App.vue'
+
+describe('App', () => {
+  const wrapper = mount(App)
+  
+  it('should render properly', () => {
+    expect(wrapper.text()).toContain('Coding Test')
+  })
+
+  it('should handle input', async () => {
+    await wrapper.find('textarea').setValue('{"a":[1,1]}')
+    const result = wrapper.find('.result')
+    expect(JSON.stringify(JSON.parse(result.text()))).toContain('{"a":[1]}')
+  })
+})
